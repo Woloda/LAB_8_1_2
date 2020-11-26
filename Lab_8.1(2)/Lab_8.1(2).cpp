d31d763b38601288a4199d04a6b75d9126da64ca
Woloda <dorozovecvoloda@gmail.com>
2020-11-26 19:48:38 +0200

    Lab_8_1_2

---
+++ Lab_8.1(2)/Lab_8.1(2).cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+bool perevirka(const char* lit_r, const char* para, const char* para1, int j, int k, int i);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char lit_r[20] = "sfSQWfs";
+	cout << "Сфомований літерний рядок: " << endl << endl << lit_r << endl << endl;
+	char para[] = "SQ";
+	char para1[] = "QS";
+
+	if (perevirka(lit_r, para, para1, 0, 0, 0))
+		cout << "Так є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+	else
+		cout << "Ні не є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+
+	return 0;
+}
+
+bool perevirka(const char* lit_r, const char* para, const char* para1, int j, int k, int i)
+{
+	j = 0;
+	k = i;
+	while (para[j] != '\0')
+	{
+		if ((lit_r[k] == para[j]) || (lit_r[k] == para1[j]) && lit_r[k] != '\0')
+		{
+			j++;
+			k++;
+		}
+		else
+			break;
+	}
+	if (para[j] == '\0')
+		return true;
+	if (lit_r[i] != '\0')
+		return perevirka(lit_r, para, para1, j, k, i + 1);
+	else
+		return false;
+
+}
---
+++ UnitTest(Lab.8.1(2))/UnitTest(Lab.8.1(2)).cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1(2)/Lab_8.1(2).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab812
+{
+	TEST_CLASS(UnitTestLab812)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			using namespace std;
+			char lit_r[20] = "sfSQWfs";
+			char para[] = "SQ";
+			char para1[] = "QS";
+
+			bool l = false;
+			l = perevirka(lit_r, para, para1, 0, 0, 0);
+			Assert::AreEqual(l, true);
+		}
+	};
+}
